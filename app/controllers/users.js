1cf23149aa0a351d4dfb25cac79f473a404e0d8e
shogochiai <shogo1104@gmail.com>
2016-03-03 16:53:31 +0900

    create callback

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,11 +1,12 @@
 export default {
   create: (req, res)=>{
     req.models.user.qFind({ facebook_id: req.body.facebook_id }).then(users=>{
-      console.log('users', users);
       if(users.length > 0){
         var user = users[0];
-        // session create
-        res.send(false);
+        res.session.id = user.id;
+        res.session.save(_=>{
+          res.send({ created: false });
+        });
       } else {
         var user = {
           facebook_id: req.body.facebook_id,
@@ -13,11 +14,11 @@ export default {
           role: "eater"
         };
         req.models.user.qCreate(user).then(items=>{
-          console.log("created user", items);
+          console.log("created user", user);
           // if success session create
-          res.session.id = items[0].id;
+          res.session.id = user.id;
           res.session.save(_=>{
-            res.send(true);
+            res.send({ created: true });
           });
         })
       }
