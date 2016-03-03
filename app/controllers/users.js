450e78b901b1ba49343c58c4d557b7136dcde9b8
shogochiai <shogo1104@gmail.com>
2016-03-03 14:40:04 +0900

    log verbose

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,5 +1,7 @@
 export default {
   create: (req, res)=>{
+    console.log('user create');
+    console.log(req.models);
     req.models.user.find({ facebook_id: req.body.facebook_id }).then(users=>{
       console.log(users);
       if(users.length > 0){
