a52ec74c9da01454dec987f123c8e14d6395a1e0
EC2 Default User <ec2-user@ip-172-31-21-80.ap-northeast-1.compute.internal>
2016-04-25 10:05:50 +0000

    mapper tweaked

--- app/controllers/_middleware/mapper.js
+++ app/controllers/_middleware/mapper.js
@@ -4,21 +4,16 @@ import _ from 'lodash';
 export default class Mapper {
   static b2f(tableName, backendObject){
     var frontendObject = {};
-    console.log(backendObject);
-    console.log(SCHEMA[tableName]);
     Object.keys(backendObject).map((key, i)=>{
       var backendFieldName = key;
       var frontendFieldName = SCHEMA[tableName][backendFieldName];
       frontendObject[frontendFieldName] = backendObject[backendFieldName];
     });
-    console.log(frontendObject);
     return frontendObject;
   }
 
   static f2b(tableName, frontendObject){
     var backendObject = {};
-    console.log(frontendObject);
-    console.log(SCHEMA[tableName]);
     Object.keys(backendObject).map((key, i)=>{
       var frontendFieldName = key;
       // retrieve firld from schema
@@ -29,7 +24,6 @@ export default class Mapper {
       }) )[0];
       backendObject[backendFieldName] = frontendObject[frontendFieldName];
     });
-    console.log(backendObject);
     return backendObject;
   }
 }
